c57b55636f78d1eac2bc78def4cb3f3f7076a0a6
nagi0101 <62392317+nagi0101@users.noreply.github.com>
2020-09-19 00:11:07 +0900

    stock done

--- stock.js
+++ stock.js
@@ -1,13 +1,16 @@
 const sidebar = document.querySelector(".stock_manage");
-const menuBtn = document.querySelector(".stock_manage > i");
+const menuBtn = document.querySelector(".btn_manage");
+const bg = document.querySelector(".black_background");
 const SIDEBAR_SHOWING = "side-bar-showing";
 const HEADER_UP_BTN = "fa-arrow-circle-up";
 const HEADER_DOWN_BTN = "fa-arrow-circle-down";
+const BG_SHOWING = "background-showing";
 
 function handleShowingSidebar() {
   sidebar.classList.toggle(SIDEBAR_SHOWING);
   menuBtn.classList.toggle(HEADER_DOWN_BTN);
   menuBtn.classList.toggle(HEADER_UP_BTN);
+  bg.classList.toggle(BG_SHOWING);
 }
 
 function handleSidebar() {
